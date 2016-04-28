7cae04698dad0560e8761585006e695b9188a075
jzlshitian <zl.jiang@outlook.com>
2016-04-29 00:26:16 +0800

    first commit

---
+++ src/main.groovy
@@ -0,0 +1,3 @@
+/**
+ * Created by jzl on 16/4/28.
+ */
---
+++ src/parsing.groovy
@@ -0,0 +1,19 @@
+/**
+ * Created by jzl on 16/4/28.
+ */
+import groovy.swing.SwingBuilder
+import javax.swing.*
+
+class parsing {
+    def well_defined = "well defined"
+    def not_well_defined = "not well defined"
+
+    void parse_input(input) {
+        if (input[0] != '(') {
+            println(not_well_defined)
+            return
+        }
+
+        
+    }
+}
