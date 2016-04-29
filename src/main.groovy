83e48e52dc2be8b680a10efdf81eac45cabbbbe2
jzlshitian <zl.jiang@outlook.com>
2016-04-29 12:58:37 +0800

    check if a proposition is well defined

--- src/main.groovy
+++ src/main.groovy
@@ -1,3 +1,11 @@
 /**
  * Created by jzl on 16/4/28.
  */
+
+def test = "(() \\eq (A_{1} \\and A_{2}))"
+
+parse = new parsing()
+
+boolean answer = parse.equal_parentheses(test) && parse.is_well_defined(test)
+
+assert answer
--- src/parsing.groovy
+++ src/parsing.groovy
@@ -5,15 +5,97 @@ import groovy.swing.SwingBuilder
 import javax.swing.*
 
 class parsing {
-    def well_defined = "well defined"
-    def not_well_defined = "not well defined"
+    def proposition_letter_1 = ~/[A-Z]/
+    def proposition_letter_2 = ~/[A-Z]_\{[0-9]\}/
+    def connectives = ["\\and", "\\or", "\\not", "\\imply", "\\eq"]
 
-    void parse_input(input) {
-        if (input[0] != '(') {
-            println(not_well_defined)
-            return
+    boolean is_well_defined(String input) {
+        if (proposition_letter_1.matcher(input).matches() || proposition_letter_2.matcher(input).matches())
+            return true
+
+        String left_proposition, right_proposition
+
+        if (input[0] != '(')
+            return false
+
+        int index = 1
+
+        if (input[1] == '(') {
+            int left_parentheses = 0, right_parentheses = 0
+            for (;index<input.length();index++) {
+                if (input[index] == '(')
+                    left_parentheses++
+                else if (input[index] == ')') {
+                    right_parentheses++
+                    if (right_parentheses == left_parentheses) {
+                        left_proposition = input[1..index]
+                        index += 2
+                        break
+                    }
+                }
+            }
+        } else if (5 < input.length() && proposition_letter_2.matcher(input[1..5]).matches()) {
+            left_proposition = input[1..5]
+            index = 5 + 2
+        } else if (proposition_letter_1.matcher(input[1]).matches()) {
+            left_proposition = input[1]
+            index = 1 + 2
+        } else
+            return false
+
+        //println(left_proposition)
+
+        if (input[index..index+3] == "\\and")
+            index += 5
+        else if (input[index..index+2] == "\\or" || input[index..index+2] == "\\eq")
+            index += 4
+        else if (input[index..index+5] == "\\imply")
+            index += 7
+        else
+            return false
+
+        int begin = index
+        if (input[begin] == '(') {
+            int left_parentheses = 0, right_parentheses = 0
+            for (;index<input.length();index++) {
+                if (input[index] == '(')
+                    left_parentheses++
+                else if (input[index] == ')') {
+                    right_parentheses++
+                    if (right_parentheses == left_parentheses) {
+                        right_proposition = input[begin..index++]
+                        break
+                    }
+                }
+            }
+        } else if (begin + 4 < input.length() && proposition_letter_2.matcher(input[begin..begin+4]).matches()) {
+            right_proposition = input[begin..begin+4]
+            index = begin + 5
+        } else if (proposition_letter_1.matcher(input[begin]).matches()) {
+            right_proposition = input[begin]
+            index = begin + 1
+        } else
+            return false
+
+        //println(right_proposition)
+
+        if (index != input.length()-1)
+            return false
+
+
+        return is_well_defined(left_proposition) && is_well_defined(right_proposition);
+    }
+
+    boolean equal_parentheses(String input) {
+        int left_parentheses = 0, right_parentheses = 0
+
+        for (int i in 0..input.length()-1) {
+            if (input[i] == '(')
+                left_parentheses++
+            else if (input[i] == ')')
+                right_parentheses++
         }
 
-        
+        return left_parentheses == right_parentheses
     }
 }
